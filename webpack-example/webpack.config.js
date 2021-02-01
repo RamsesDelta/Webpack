fcf38b7d4a3c2ef57c299f2dc984f873c84e3c2b
unknown <deltaghost.ram4@gmail.com>
2021-02-01 16:04:11 -0600

    first commit

---
+++ webpack-example/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                },
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        })
+    ]
+}
